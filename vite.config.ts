6693aab61cca81872e7cb66ce2f85e02d3295926
jhjwjn <106360683+jhjwjn@users.noreply.github.com>
2026-03-23 04:44:13 +0900

    Fix base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/newcalendarapp-main/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
